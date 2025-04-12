a7893c99cfb3bdfbb1589fcfea7189ee50f196ef
pm-azhar-mulla <azhar@L2047.local>
2025-04-12 09:50:34 +0530

    Refactored the code

--- modules/owCustomizedAdsModule.js
+++ modules/owCustomizedAdsModule.js
@@ -6,28 +6,56 @@ const MODULE_NAME = 'owCustomizedAds';
 function handleConfig(config) {
   config = config.OWCustomizedAds;
   logInfo(`${MODULE_NAME} : Setting config`);
-  if (config && config.stickyAds && config.stickyAds.enabled) {
-    var divs = config.stickyAds.divs;
-    var divSelector = '';
-    divs.forEach(function(iDiv) {
-      divSelector += `#${iDiv} > iframe,`;
-    })
-    divSelector = divSelector.slice(0, -1);
-    let css = `${divSelector} {position: sticky;top: 20px;}`;
-    let head = document.head || document.getElementsByTagName('head')[0];
-    let style = document.createElement('style');
-
-    head.appendChild(style);
-
-    style.type = 'text/css';
-
-    if (style.styleSheet) {
-      // This is required for IE8 and below.
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
+  
+  // Only process if we have a valid config
+  if (!config) return;
+  
+  // Define feature handlers
+  const featureHandlers = {
+    stickyAds: function(stickyConfig) {
+      if (!stickyConfig.enabled || !stickyConfig.divs || !stickyConfig.divs.length) return;
+      
+      // Create CSS selector from all divs in one go
+      const divSelector = stickyConfig.divs.map(div => `#${div} > iframe`).join(',');
+      
+      // Create and append style element in a more streamlined way
+      const style = document.createElement('style');
+      style.type = 'text/css';
+      const css = `${divSelector} {position: sticky;top: 20px;}`;
+      
+      // Handle different browser compatibility in a cleaner way
+      style.styleSheet ? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));
+      
+      // Append to head
+      (document.head || document.getElementsByTagName('head')[0]).appendChild(style);
+    },
+    
+    carouselAds: function(carouselConfig) {
+      if (!carouselConfig.enabled) return;
+      handleCarouselAds(carouselConfig);
     }
-  }
+  };
+  
+  // Process each feature if it exists in the config
+  Object.keys(featureHandlers).forEach(feature => {
+    if (config[feature]) {
+      featureHandlers[feature](config[feature]);
+    }
+  });
+}
+
+/**
+ * Dummy function to handle carousel ads
+ * @param {Object} carouselConfig - The carousel ads configuration
+ * @param {string} carouselConfig.adUnitCode - The ad unit code
+ * @param {string} carouselConfig.divId - The div ID for the carousel
+ * @param {number} carouselConfig.duration - The duration between slides
+ * @param {number} carouselConfig.slidesCount - The number of slides in the carousel
+ */
+function handleCarouselAds(carouselConfig) {
+  logInfo(`${MODULE_NAME} : Setting up carousel ads with config:`, carouselConfig);
+  // This is a dummy function that will be expanded later
+  // It will handle the carousel ads functionality
 }
 
 // handleSetDebugConfig will be called whenever setConfig is called with debug property
@@ -57,6 +85,13 @@ config.getConfig('owCustomizedAds', config => handleConfig(config));
 //           stickyAds: {
 //             enabled: true,
 //             divs: [iDiv.id]
+//           },
+//           carouselAds: {
+//             enabled: true,
+//             adUnitCode: "/43743431/DMDemo",
+//             divId: iDiv.id,
+//             duration: 3000,
+//             slidesCount: 5
 //           }
 //          }
 //       });
